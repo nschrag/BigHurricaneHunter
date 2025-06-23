79e742930020f734b855d1c2ba243abcad24acd2
nschrag <nschrag@gmail.com>
2025-06-23 14:56:36 -0500

    New map and particle hurricanes

---
+++ Rotate.gd
@@ -0,0 +1,12 @@
+@tool
+extends Node2D
+
+@export var active: bool
+@export var rate: float
+
+func _process(delta: float) -> void:
+	if !active:
+		rotation = 0
+		return
+		
+	rotation += PI * delta
