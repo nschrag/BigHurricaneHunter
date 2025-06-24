e0ab30eceaa9bfc975fb3c248ac14267541ac161
nschrag <nschrag@gmail.com>
2025-06-24 14:18:06 -0500

    Added hit streak

--- Reticule.gd
+++ Reticule.gd
@@ -1,16 +1,23 @@
 extends Node2D
 
 @export var speed = 300
+
 @export var hurricane_parent: Node2D
+
 @onready var horizontal = $Horizontal
 @onready var vertical = $Vertical
 @onready var center = $Center
+
 
 signal charge_level_changed(value: int)
 
 var charge: float = 0
 var charge_level: int = 0
 @export var max_charge_level: int = 5
+@export var full_charge_time: float = 2
+@onready var charge_rate = full_charge_time / max_charge_level
+
+var combo_level: int = 0
 
 func get_speed() -> float:
 	if Input.is_action_pressed("fire"):
@@ -28,21 +35,28 @@ func _process(delta: float) -> void:
 	vertical.position = Vector2(center.position.x, 0)
 	
 	if Input.is_action_pressed("fire"):
-		charge += delta
+		charge += charge_rate * delta
 		if charge * max_charge_level > charge_level + 1:
 			charge_level = charge * max_charge_level
 			charge_level_changed.emit(charge_level)
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_released("fire"):
-		charge = 0
-		charge_level = 0
-		charge_level_changed.emit(charge_level)
-		
+		var success = false
 		for h in hurricane_parent.get_children():
 			if h is Hurricane:
 				var target_pos = Vector2(vertical.position.x, horizontal.position.y)
 				var distance = (target_pos - h.position).length()
-				if distance < 6:
+				if distance <= 10:
+					success = true
 					h.queue_free()
 					print("hit")
+		
+		if success:
+			combo_level += 1
+		else:
+			combo_level = 0
+			
+		charge_level = combo_level
+		charge = charge_level / float(max_charge_level)
+		charge_level_changed.emit(charge_level)
